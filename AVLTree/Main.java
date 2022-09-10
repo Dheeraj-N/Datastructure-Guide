83dc1a2c8eed14558a62c46f217387a2090b2166
Dheeraj-N <iamdheeraj2001@gmail.com>
2022-09-10 23:19:39 +0530

    Added AVLTree

---
+++ AVLTree/Main.java
@@ -0,0 +1,19 @@
+package AVLTree;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		AVLTree  avl = new AVLTree();
+		avl.insert(8);
+		avl.insert(5);
+		avl.insert(14);
+		avl.insert(3);
+		avl.insert(7);
+		avl.insert(10);
+		avl.insert(2);
+		avl.insert(1);
+	}
+
+}
